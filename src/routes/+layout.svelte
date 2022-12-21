2751344cad18c957f389df2e4036643935c4c07a
eur2 <gregory.dapra@gmail.com>
2022-12-21 10:35:39 +0100

    pkg update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,17 @@
 <script>
-	import '../app.css';
+	import './styles.css';
 	export let data;
 	const { header, subscribe } = data;
-	import Header from "$lib/Header.svelte";
-import Subscribe from "$lib/Subscribe.svelte";
-import Logo from "$lib/Logo.svelte";
+	import Header from '$lib/Header.svelte';
+	import Subscribe from '$lib/Subscribe.svelte';
+	import Logo from '$lib/Logo.svelte';
 </script>
 
 <Header>
-{@html header.content.rendered}
+	{@html header.content.rendered}
 </Header>
-  <slot></slot>
-  <Logo/>
+<slot />
+<Logo />
 <Subscribe>
-  {@html subscribe.content.rendered}
+	{@html subscribe.content.rendered}
 </Subscribe>
-
