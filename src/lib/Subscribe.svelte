1b4f583ab4ef29057c2fe9df3d8042db20d7e7ca
eur2 <gregory.dapra@gmail.com>
2023-09-11 17:37:56 +0200

    info update

--- src/lib/Subscribe.svelte
+++ src/lib/Subscribe.svelte
@@ -12,8 +12,8 @@
 			style="background-color:black;color:fuchsia;"
 			rel="noreferrer"
 			target="_blank"
-			href="https://www.helloasso.com/associations/maison-de-l-architecture-occitanie-pyrenees/collectes/200-plan-libre-anatomie-d-un-journal-d-architecture-en-prevente 
-		">+++Prévente <i>200 Plan Libre+++</i></a
+			href="https://maop.fr/publications/plan-libre-anatomie-d-un-journal-d-architecture"
+			>+++<i>200 Plan Libre+++</i>, le livre</a
 		>
 		<button class="p251251 flex1 center {visible ? 'none' : ''}" on:click={handleToggle}>
 			+++Abonnement à <i>Plan Libre</i> 25€+++
@@ -22,7 +22,7 @@
 			<div class="p">
 				<div class="flex jc-sb">
 					<span />
-					<h3>+++Abonnement annuel/10 numéros/25€+++</h3>
+					<h3>+++Abonnement annuel/4 numéros/25€+++</h3>
 					<button on:click={handleToggle}>×</button>
 				</div>
 				<slot />
